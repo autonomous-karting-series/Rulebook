ee8e8ae6e23e5443dbf355890e5c2b2c218e6505
agoeden12 <agoeden12@users.noreply.github.com>
2026-02-04 03:29:00 +0000

    deploy: e094ece703f68f2bc052bb03a2721a1516861c6e

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0b0e02bf.js',
+                path_to_root + 'searchindex-5c7e6edf.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
